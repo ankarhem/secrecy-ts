5b37952db0e7c997dd9f8b27b77994b342f16be6
Jakob Ankarhem <jakob@ankarhem.dev>
2024-11-08 22:29:26 +0100

    move console test to console_test file

--- console_test.ts
+++ console_test.ts
@@ -1,3 +1,23 @@
+import { assertEquals } from "@std/assert";
 import { Secret } from "./mod.ts";
 
 console.log(new Secret("password"));
+
+Deno.test("Secret - hidden from stdout and stderr", async () => {
+  // spawn a process and verify that stdout is redacted
+  const command = new Deno.Command("deno", {
+    args: ["console_test.ts"],
+    stdout: "piped",
+    stderr: "piped",
+    stdin: "piped",
+  });
+  const child = command.spawn();
+  child.output().then((value) => {
+    const output = new TextDecoder().decode(value.stdout);
+    assertEquals(output, "[REDACTED]\n");
+  });
+
+  const status = await child.status;
+  assertEquals(status.success, true);
+  child.stdin.close();
+});
--- mod_test.ts
+++ mod_test.ts
@@ -21,25 +21,6 @@ Deno.test("Secret - hidden in serialization", () => {
   );
 });
 
-Deno.test("Secret - hidden from stdout and stderr", async () => {
-  // spawn a process and verify that stdout is redacted
-  const command = new Deno.Command("deno", {
-    args: ["console_test.ts"],
-    stdout: "piped",
-    stderr: "piped",
-    stdin: "piped",
-  });
-  const child = command.spawn();
-  child.output().then((value) => {
-    const output = new TextDecoder().decode(value.stdout);
-    assertEquals(output, "[REDACTED]\n");
-  });
-
-  const status = await child.status;
-  assertEquals(status.success, true);
-  child.stdin.close();
-});
-
 Deno.test("Secret - hidden property accessibility", () => {
   const secret = new Secret("password");
   // deno-lint-ignore no-explicit-any
