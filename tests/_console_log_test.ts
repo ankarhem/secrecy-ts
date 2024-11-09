8f213f83470c1a11338939988eef64e66290d84b
Jakob Ankarhem <jakob@ankarhem.dev>
2024-11-09 13:22:48 +0100

    remove if throw

--- tests/_console_log_test.ts
+++ tests/_console_log_test.ts
@@ -1,11 +1,5 @@
 import { assertEquals } from "@std/assert";
 
-const isDeno = globalThis.Deno?.version?.deno != null;
-
-if (!isDeno) {
-  throw new Error("This test can only be run in Deno");
-}
-
 Deno.test("Secret - hidden from stdout and stderr", async () => {
   // spawn a process and verify that stdout is redacted
   const command = new Deno.Command("deno", {
